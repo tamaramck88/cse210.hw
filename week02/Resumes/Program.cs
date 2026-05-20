753c55dd5fae6aea4114c0613d3889ec30d72066
tamaramck88 <tmcknight88@yahoo.com>
2026-05-20 20:11:23 +0200

    changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,3 +7,66 @@ class Program
         Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
+using System;
+using System.Collections.Generic;
+
+public class Job
+{
+    public string _jobTitle = "";
+    public string _company = "";
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create jobs
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        // Add jobs
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display resume
+        myResume.Display();
+    }
+}
+</Project>
