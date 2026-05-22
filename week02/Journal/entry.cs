7d27e6c823f3429b68429566778dd0311548670d
tamaramck88 <tmcknight88@yahoo.com>
2026-05-22 17:01:57 +0200

    changes

--- week02/Journal/entry.cs
+++ week02/Journal/entry.cs
@@ -2,10 +2,10 @@ using System;
 
 public class Entry
 {
-    public string _date;
-    public string _promptText;
-    public string _entryText;
-    public string _mood;
+    public string? _date;
+public string? _promptText;
+public string? _entryText;
+public string? _mood;
 
     public void Display()
     {
